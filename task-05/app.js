9ef29f3e1b46aab57c5aad2b75950a76b630ad7d
Arnold <antaloczi.arnold@gmail.com>
2021-07-05 16:32:58 +0200

    Task 5 done

--- task-05/app.js
+++ task-05/app.js
@@ -1,13 +1,26 @@
 /**
  * 1. Töltsd be a szükséges modulokat: http és az általad írt router modul.
  */
+const http = require('http');
+const router = require('./router/countryRouter');
 
 // 2. Definiáld a port értékét 8080 -ra a port változóban.
+const port = 8080;
 
 /**
  * 3. Hozz létre egy http szervert.
  * A szerver get kérés esetén meghívja a router kapcsolódó metódusát, 
  * amely válaszol a kérésre.
  */
+const server = http.createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    const list = await router['GET']();
+    res.setHeader('Content-Type', 'application/json');
+    res.end(list);
+  }
+});
 
 // 4. Állítsd be, hogy a szerver figyelje a port változóban definiált portot.
+server.listen(port, async () => {
+  console.log(`Backend server is running at: http://localhost:${port}`);
+});
